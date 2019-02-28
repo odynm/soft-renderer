91fb96d5d52616132d8ecf921b8de6458500a372
Mathias Luiz Ody <0176416@feevale.br>
2019-02-28 20:24:28 -0300

    Add support for community VS

--- Code/build.bat
+++ Code/build.bat
@@ -5,6 +5,7 @@ pushd ..\Build
 
 REM if cl compiler is not a defined var, call vcvars setup and hope it is in the same folder for everyone
 IF NOT DEFINED cl call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\VC\Auxiliary\Build\vcvars32"
+IF NOT DEFINED cl call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars32"
 
 cl -DHANDMADE_WIN32=1 -FC -Zi ..\Code\Win32_Renderer.cpp ..\Code\Model.cpp ..\Code\Std.cpp ..\Code\MathLib.cpp  user32.lib gdi32.lib
 popd
